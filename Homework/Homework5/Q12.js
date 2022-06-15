d358a9897f0463246f1ec659ba9c8ee53ec4542a
Chieh0622 <99935364+Chieh0622@users.noreply.github.com>
2022-06-16 00:07:01 +0800

    Update Q12.js

--- Homework/Homework5/Q12.js
+++ Homework/Homework5/Q12.js
@@ -23,6 +23,9 @@ class Ratio
 }
 var r1 = new Ratio(1, 5), r2 = new Ratio(3, 4); 
 var r3 = r1.add(r2); 
+var r4 = r1.sub(r2);
 console.log(r3.toString());
+console.log(r4.toString());
 
 // 19/20
+// -11/20
