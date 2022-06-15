366bf878edd3e246078f82f4e170a3eefa4dcf2e
Chieh0622 <99935364+Chieh0622@users.noreply.github.com>
2022-06-15 23:29:30 +0800

    Create Q11.js

---
+++ Homework/Homework5/Q11.js
@@ -0,0 +1,17 @@
+class Ratio
+{
+    constructor (num1, num2)
+    {
+        this.a = num1;
+        this.b = num2;
+    }
+
+    tostring ()
+    {
+        return this.a + "/" + this.b;
+    }
+}
+var r1 = new Ratio(1, 5);
+console.log(r1.tostring());
+
+//1/5
