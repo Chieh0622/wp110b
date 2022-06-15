8dc4823b6ce4baa3ef9c753199b649110f441351
Chieh0622 <99935364+Chieh0622@users.noreply.github.com>
2022-06-16 00:00:40 +0800

    Create Q12.js

---
+++ Homework/Homework5/Q12.js
@@ -0,0 +1,29 @@
+class Ratio
+{
+    constructor (num1, num2)
+    {
+        this.a = num1;
+        this.b = num2;
+    }
+
+    tostring ()
+    {
+        return this.a + '/' + this.b;
+    }
+
+    add (x)
+    {
+        return this.a * x.b + this.b * x.a + "/" + this.b * x.b;
+    }
+
+    sub (x)
+    {
+        return this.a * x.b - this.b * x.a + "/" + this.b * x.b;
+    }
+
+}
+var r1 = new Ratio(1, 5), r2 = new Ratio(3, 4); 
+var r3 = r1.add(r2); 
+console.log(r3.toString());
+
+// 19/20
